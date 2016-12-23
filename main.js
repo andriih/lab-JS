a41e1ac5adc2bf93f9fb884a18e18036eac56768
Your Name <you@example.co>
2016-12-23 21:32:36 +0200

    last commit 23.12.2016

--- main.js
+++ main.js
@@ -19,27 +19,51 @@
 //
 //
 // console.log(s);
-var a=[],
-    number=0;
 
-do{
-    number = prompt("Number:");
-    if(number != "q"){
-        a.push(parseInt(number));
-    }else{
-        alert("Done");
-        break;
+
+
+// var a=[],
+//     number=0;
+//
+// do{
+//     number = prompt("Number:");
+//     if(number != "q"){
+//         a.push(parseInt(number));
+//     }else{
+//         alert("Done");
+//         break;
+//     }
+// }while(true);
+//
+// alert(a);
+// console.log(a);
+//
+// var start = +prompt("What to change");
+// var changeTo = +prompt("change to...");
+// var index = a.indexOf(start);
+//
+// console.log("Index "+index);
+// var res = a.splice(index,1,changeTo);
+//
+
+// console.log(a);
+
+
+
+//var res = a.join('-');
+//var res = a.reverse();
+//var res = a.sort();
+
+
+var a =[];
+var car = {};
+for(var i=1; i<=5; i++ ){
+    var label  = prompt("Choose your car");
+    car = {
+        model: label,
     }
-}while(true);
-
-alert(a);
-console.log(a);
-
-var start = +prompt("What to change");
-var changeTo = +prompt("change to...");
-var index = a.indexOf(start);
-
-console.log("Index "+index);
-var res = a.splice(index,1,changeTo);
+    a.push(car);
+    console.log(car);
+}
 
 console.log(a);
