75afb78c96239d94f9fca5455d62847541666dfc
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2016-12-28 09:58:34 +0200

    done

--- dz26.12.js
+++ dz26.12.js
@@ -1,5 +1,5 @@
 //var a = ['one','two','three','four','five'];
-var a = [1,2,3,4,5,6,7,8,9,10];
+var a = [1,2,303,4,5,6,7,8,9,10];
 //first function
 function outputArrayInOne (arr){
     var newArr = [];
@@ -28,17 +28,39 @@ function outputArrayStep (arr,step){
     return newArr;
 }
 //third function
-function smallestInArr (arr,step){
+function smallestInArr (arr){
     var newArr = [];
+    var small = arr[0];
+
     if(arr.length === 0){
         console.log('Array is empty!');
     }else{
-        for(var i=0; i <=arr.length-1; i++ ){
-            for(var j=0;j <= arr.length-1;j+=2){
-
+        for(var i=1; i <= arr.length; i++ ){
+            if(arr[i] < small){
+                small = arr[i];
             }
         }
     }
-    return newArr;
+    
+    return ("smallest number "+small);
 }
+//Largest element
+function largestInArr (arr){
+    var newArr = [];
+    var large = arr[0];
+
+    if(arr.length === 0){
+        console.log('Array is empty!');
+    }else{
+        for(var i=1; i <= arr.length; i++ ){
+            if(arr[i] > large){
+                large = arr[i];
+            }
+        }
+    }   
+    return ("largest number "+large);
+}
+console.log(outputArrayInOne(a));
+console.log(outputArrayStep(a,3));
 console.log(smallestInArr(a));
+console.log(largestInArr(a));
